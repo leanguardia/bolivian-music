93577b9e8edbbb12963850f37c7b4b6790832b17
Leandro Guardia <lean.guardia93@gmail.com>
2020-07-03 19:41:32 +0100

    Feat: Store number of Followers

--- data/spotify_collector.py
+++ data/spotify_collector.py
@@ -4,17 +4,23 @@ import pandas as pd
 
 spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
 
-def fetch_playlists():
+def fetch_playlists(min_followers=0):
     results = spotify.search(q='bolivia', type='playlist', limit=50)
     playlists = results['playlists']['items']
     data = _build_playlist_data()
-    
-    for playlist in playlists:
-        data['id'].append(playlist['id'])
-        data['name'].append(playlist['name'])
-        data['owner_name'].append(playlist['owner']['display_name'])
-        data['owner_id'].append(playlist['owner']['id'])
-        data['description'].append(playlist['description'])
+
+    for playlist_simple in playlists:
+        playlist = spotify.playlist(playlist_simple['id'])
+        n_followers = playlist['followers']['total']
+        if n_followers >= min_followers:
+            data['id'].append(playlist['name'])
+            data['name'].append(playlist['name'])
+            data['followers'].append(n_followers)
+            data['owner_name'].append(playlist['owner']['display_name'])
+            data['owner_id'].append(playlist['owner']['id'])
+            data['description'].append(playlist['description'])
+        else:
+            print('Discarding', playlist['id'])
     
     df = pd.DataFrame(data, columns=data.keys())
     return df
@@ -23,9 +29,10 @@ def _build_playlist_data():
     return {
         'id': [],
         'name': [],
-        'description': [],
+        'followers': [],
         'owner_name': [],
         'owner_id': [],
+        'description': [],
     }
 
 if __name__ == "__main__":
