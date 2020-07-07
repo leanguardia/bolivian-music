0db4287b960d22a2e22098fea054d4716e796edf
Leandro Guardia <lean.guardia93@gmail.com>
2020-07-08 00:14:46 +0100

    Feat: Web Hello World

---
+++ app/server.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import Flask
+from flask import render_template, request, jsonify
+
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def home():
+  return 'Hola Mundo!'
+
+
+def main():
+    app.run(host='0.0.0.0', port=3003, debug=True)
+
+
+if __name__ == '__main__':
+    main()
