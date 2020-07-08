992a6f9e5b4564b91759b20be842050d0bdfd706
Leandro Guardia <lean.guardia93@gmail.com>
2020-07-08 21:46:50 +0100

    Chore: Add Glide to the library

--- app/static/main.js
+++ app/static/main.js
@@ -1,1 +1,11 @@
-console.log("holamundo!");
+$(document).ready(function() {
+  console.log("Documen Ready!");
+
+  const config = {
+    type: 'carousel',
+    perView: 2,
+  };
+
+  new Glide('.glide', config).mount();
+
+});
