c7b33776f0ff8651e2d351f83e364bf530f9c4b9
Leandro Guardia <lean.guardia93@gmail.com>
2020-07-09 08:15:05 +0100

    Feat: Show Top Grid

--- app/server.py
+++ app/server.py
@@ -4,13 +4,19 @@ from flask import render_template, request, jsonify
 
 import pandas as pd
 import numpy as np
+from ast import literal_eval
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 def home():
-  return render_template('home.html')
+  artists = pd.read_csv('data/artists_grouped.csv')
+  artists.genres = artists.genres.apply(literal_eval)
+  ranked_artists = {}
+  for group in artists.groupby('ranking'):
+     ranked_artists[group[0]] = group[1].to_dict('records')
+  return render_template('home.html', rank_artists=ranked_artists)
 
 
 def main():
